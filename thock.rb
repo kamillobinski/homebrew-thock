e573f666b634b176aa628c207a72910c66180874
kamillobinski <kamillobinski@gmail.com>
2025-03-27 06:39:55 +0100

    chore: update formula

--- thock.rb
+++ thock.rb
@@ -1,9 +1,9 @@
 class Thock < Formula
   desc "Thock your mac keyboard"
   homepage "https://github.com/kamillobinski/thock"
-  url "https://github.com/kamillobinski/thock/releases/download/1.9.2/Thock-1.9.2.zip"
-  sha256 "e8ab5ecbe9851cd3badce7bc09872fd1c6affed3f4dd54c5d75e1bd94cd35616"
-  version "1.9.2"
+  url "https://github.com/kamillobinski/thock/releases/download/1.9.3/Thock-1.9.3.zip"
+  sha256 "1adf840ddb33629f422bc5e2f01183c9c0388f3cd7b100191c9334961179a89f"
+  version "1.9.3"
 
   def install
     prefix.install "Thock.app"
